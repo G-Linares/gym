8a076d954d02641597d460ae1af5c2df92c4ce69
G-Linares <gerrylinares@gmail.com>
2022-06-14 14:44:00 -0500

    added carousel

--- src/components/BodyPart.jsx
+++ src/components/BodyPart.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import {Stack, Typography} from "@mui/material";
+import Icon from '../assets/images/icon.png'
+// importa todas los iconos
 
-const BodyPart = () => {
+const BodyPart = ({item, setBodyPart, bodyPart}) => {
   return (
-    <div>BodyPart</div>
+    <Stack 
+      type="button"
+      alignItems="center"
+      justifyContent="center"
+      className='bodyPart-card'
+      sx={{
+          borderTop: bodyPart === item ? '4px solid #ff2625' : '',
+          backgroundColor: '#fff',
+          borderBottomLeftRadius: '20px',
+          width:'270px',
+          height:'280px',
+          cursor:"pointer",
+          gap:'47px',
+
+      }}
+    >
+      <img src={Icon} alt="dumbbell" style={{width:'80px',height:'80px'}}/>
+      <Typography>{item}</Typography>
+    </Stack>
   )
 }
 
