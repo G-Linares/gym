434e88cd03293670612745c31e1a2c709bba3b05
G-Linares <gerrylinares@gmail.com>
2022-06-15 13:07:19 -0500

    final touches

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Stack } from '@mui/material';
-import {CirclesWithBar} from "react-loader-spinner";
+import {Rings} from "react-loader-spinner";
 
 const Loader = () => {
   return (
     <Stack direction="row" justifyContent="center" alignItems="center" width="100%">
-        <CirclesWithBar color="gray" />
+        <Rings color="red" height={80} width={80} />
     </Stack>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 
 import App from './App';
+import ScrollToTop from './utils/ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
 );
